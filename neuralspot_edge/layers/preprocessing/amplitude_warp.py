9235ffff9db44b6e80f520591fd64f18a8591e5e
Adam Page <apage224@gmail.com>
2024-07-31 09:54:21 -0500

    Add amplitude warping.

---
+++ neuralspot_edge/layers/preprocessing/amplitude_warp.py
@@ -0,0 +1,117 @@
+import keras
+
+from .base_augmentation import BaseAugmentation1D
+from ...utils import parse_factor
+
+
+class AmplitudeWarp(BaseAugmentation1D):
+    sample_rate: float
+    frequency: tuple[float, float]
+    amplitude: tuple[float, float]
+    noise_type: str
+
+    def __init__(
+        self,
+        sample_rate: float = 1,
+        frequency: float | tuple[float, float] = 100,
+        amplitude: float | tuple[float, float] = 0.1,
+        **kwargs,
+    ):
+        """Apply amplitude warping to the 1D input.
+        Time points are first generated at given frequency resolution with amplitude picked from normal distribution.
+        These points are then interpolated to match the input duration and multiplied to the input.
+
+        Args:
+            sample_rate (float): Sample rate of the input.
+            frequency (float|tuple[float,float]): Frequency of the warping in Hz. If tuple, frequency is randomly picked between the values.
+            amplitude (float|tuple[float,float]): Amplitude of the warping. If tuple, amplitude is randomly picked between the values.
+
+        Example:
+        ```python
+            sample_rate = 100 # Hz
+            duration = 3*sample_rate # 3 seconds
+            sig_freq = 10 # Hz
+            sig_amp = 1 # Signal amplitude
+            noise_freq = (1, 2) # Noise frequency range
+            amplitude = (0.5, 2) # Noise amplitude range
+            x = sig_amp*np.sin(2*np.pi*sig_freq*np.arange(duration)/sample_rate).reshape(-1, 1)
+            lyr = RandomNoiseDistortion1D(sample_rate=sample_rate, frequency=noise_freq, amplitude=noise_amp)
+            y = lyr(x, training=True)
+        ```
+        """
+
+        super().__init__(**kwargs)
+
+        self.sample_rate = sample_rate
+        self.frequency = parse_factor(frequency, min_value=None, max_value=sample_rate / 2, param_name="frequency")
+        self.amplitude = parse_factor(amplitude, min_value=0, max_value=None, param_name="amplitude")
+
+    def get_random_transformations(self, input_shape: tuple[int, int, int]):
+        """Generate noise distortion tensor
+
+        Args:
+            input_shape (tuple[int, ...]): Input shape.
+
+        Returns:
+            dict: Dictionary containing the noise tensor.
+        """
+        batch_size = input_shape[0]
+        duration_size = input_shape[self.data_axis]
+        ch_size = input_shape[self.ch_axis]
+
+        # Add one period to the noise and clip later
+        if self.frequency[0] == self.frequency[1]:
+            frequency = self.frequency[0]
+        else:
+            frequency = keras.random.uniform(
+                shape=(), minval=self.frequency[0], maxval=self.frequency[1], seed=self._random_generator
+            )
+        if self.amplitude[0] == self.amplitude[1]:
+            amplitude = self.amplitude[0]
+        else:
+            amplitude = keras.random.uniform(
+                shape=(), minval=self.amplitude[0], maxval=self.amplitude[1], seed=self._random_generator
+            )
+
+        warp_duration = keras.ops.cast((duration_size / self.sample_rate) * frequency + frequency, dtype="int32")
+
+        if self.data_format == "channels_first":
+            warp_shape = (batch_size, 1, ch_size, warp_duration)
+        else:
+            warp_shape = (batch_size, 1, warp_duration, ch_size)
+
+        warp_pts = keras.random.normal(warp_shape, stddev=amplitude, seed=self._random_generator)
+
+        # keras.ops doesnt contain any low-level interpolate. So we leverage the
+        # image module and fix height to 1 as workaround
+        noise = keras.ops.image.resize(
+            warp_pts,
+            size=(1, duration_size),
+            interpolation="bicubic",
+            crop_to_aspect_ratio=False,
+            data_format=self.data_format,
+        )
+        # Remove height dimension
+        noise = keras.ops.squeeze(noise, axis=1)
+        return {"noise": noise}
+
+    def augment_samples(self, inputs) -> keras.KerasTensor:
+        """Augment all samples in the batch as it's faster."""
+        samples = inputs[self.SAMPLES]
+        if self.training:
+            noise = inputs[self.TRANSFORMS]["noise"]
+            return samples + noise
+        return samples
+
+    def get_config(self):
+        """Serialize the layer configuration to a JSON-compatible dictionary."""
+        config = super().get_config()
+        config.update(
+            {
+                "sample_rate": self.sample,
+                "frequency": self.frequency,
+                "amplitude": self.amplitude,
+                "noise_type": self.noise_type,
+            }
+        )
+        return config
