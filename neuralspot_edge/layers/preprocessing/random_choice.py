92848d885495ebafdfa9870bf4890036009cb65b
Adam Page <apage224@gmail.com>
2024-07-31 09:55:07 -0500

    feat: Add RandomChoice augmentation.

---
+++ neuralspot_edge/layers/preprocessing/random_choice.py
@@ -0,0 +1,51 @@
+import keras
+from .base_augmentation import BaseAugmentation
+
+
+class RandomChoice(BaseAugmentation):
+
+    layers: list[BaseAugmentation]
+    batchwise: bool
+
+    def __init__(
+        self,
+        layers: list[BaseAugmentation],
+        batchwise: bool = False,
+        **kwargs
+    ):
+        """Randomly choose one augmentation layer from a list of augmentation layers.
+
+        Args:
+            layers (list[BaseAugmentation]): List of augmentation layers to choose from.
+            batchwise (bool): If True, apply same layer to all samples in the batch.
+        """
+        super().__init__(**kwargs)
+        self.layers = layers
+        self.batchwise = batchwise
+
+    def batch_augment(self, inputs):
+        """Apply random layer(s) to the batch"""
+        # If batchwise, apply the same layer to all samples in the batch
+        if self.batchwise:
+            lyr_idx: int = keras.random.randint(shape=(), minval=0, maxval=len(self.layers), dtype="int32", seed=self._random_generator)
+            branch_fns = [
+                lambda x: layer.call(x, training=self.training)
+                for layer in self.layers
+            ]
+            return keras.ops.switch(
+                lyr_idx,
+                branch_fns,
+                inputs
+            )
+        raise NotImplementedError("Batchwise=False is not implemented yet")
+
+    def get_config(self):
+        """Serializes the configuration of the layer."""
+        config = super().get_config()
+        config.update(
+            {
+                "layers": self.layers,
+                "batchwise": self.batchwise,
+            }
+        )
+        return config
