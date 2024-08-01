80bfddc1e46252674f9d784262cb371ffb25719b
Adam Page <apage224@gmail.com>
2024-08-01 18:54:48 +0000

    Add template path resolver.

--- neuralspot_edge/utils/__init__.py
+++ neuralspot_edge/utils/__init__.py
@@ -1,5 +1,5 @@
 from .env import env_flag, setup_logger, silence_tensorflow
-from .file import download_file, load_pkl, save_pkl, compute_checksum
+from .file import download_file, load_pkl, save_pkl, compute_checksum, resolve_template_path
 from .preprocessing import parse_factor, convert_inputs_to_tf_dataset, create_interleaved_dataset_from_generator, create_dataset_from_data
 from .rng import set_random_seed
 from .tensor import matches_spec
